d38b497eeeda307c4e0bdf48729fdffd5d6d8d23
jinmuk <xhakxh156@gmail.com>
2022-12-24 16:59:26 +0900

    [add] HelloMaven.java add HelloMaven.java 메이븐 플러그인 로직 완성

---
+++ maven_plugin/hello-maven-plugin/src/main/java/org/example/HelloMaven.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+@Mojo(name = "helloMaven")
+public class HelloMaven extends org.apache.maven.plugin.AbstractMojo {
+
+    @Parameter(name = "msg", defaultValue = "hello Maven!")
+    private String msg;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info(this.msg);
+    }
+}
